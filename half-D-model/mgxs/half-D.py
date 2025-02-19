ed168c2c945ba0582b60707829526acf7317ac2e
Nathan Glaser <nglaser3@illinois.edu>
2025-02-19 09:33:38 -0600

    update mgxs model, geom

--- half-D-model/mgxs/half-D.py
+++ half-D-model/mgxs/half-D.py
@@ -1,36 +1,37 @@
 import openmc
 import openmc.mgxs as mgxs
 import numpy as np
+import matplotlib.pyplot as plt
 
 model = openmc.Model()
 materials = openmc.Materials.from_xml('../msbr/msbr-materials.xml')
 
-homogenous = openmc.Material(name = 'homogenous')
+'''homogenous = openmc.Material(name = 'homogenous')
 dens = 0.0
 for mat in materials:
-    if mat.name == 'graphite':
-        pass
-    else:
+    if mat.name == 'fuel':
         for nuc, ao in mat.get_nuclide_atom_densities().items():
             homogenous.add_nuclide(nuc, ao)
         dens += mat.density
         dens_units = mat.density_units
-homogenous.set_density(dens_units, dens/2)
-homogenous.add_s_alpha_beta('c_Graphite',.5)
-homogenous.volume = 3.14*9**2*20
-model.materials = openmc.Materials([homogenous])
+homogenous.set_density(dens_units, dens)
+homogenous.volume = 48710000.0
+'''
+fuel = materials[0]
+fuel.add_nuclide('U233',0.025,'wo')
+model.materials = openmc.Materials([fuel])
 
-bottom, top = [openmc.ZPlane(z0 = v, boundary_type = 'vacuum') for v in [-10,10]] 
-cyl = openmc.ZCylinder(r=9,boundary_type = 'reflective')
-cell = openmc.Cell(name = 'core', fill = homogenous, region= -cyl & +bottom & -top)
+bottom, top = [openmc.ZPlane(z0 = v, boundary_type = 'reflective') for v in [-1000,1000]] 
+cyl = openmc.ZCylinder(r=900,boundary_type = 'reflective')
+cell = openmc.Cell(name = 'core', fill = fuel, region= -cyl & +bottom & -top)
 uni = openmc.Universe(cells = [cell])
 model.geometry.root_universe = uni
 
-model.settings.particles = 1000
+model.settings.particles = 10000
 model.settings.batches = 100
 model.settings.inactive = 50
-model.settings.source = openmc.IndependentSource(space=openmc.stats.Box([-9, -9, -10], 
-                                                                        [9, 9, 10]), constraints = {'fissionable':True})
+model.settings.source = openmc.IndependentSource(space=openmc.stats.Box([-900, -900, -1000], 
+                                                                        [900, 900, 1000]), constraints = {'fissionable':True})
 
 groups = mgxs.EnergyGroups(group_edges=np.array([0., 20.0e6]))
 fission = mgxs.FissionXS(domain=cell, energy_groups=groups, nu = True)
@@ -42,4 +43,24 @@ tallies += absorption.tallies.values()
 
 model.tallies = tallies
 
-model.run(output=False)
+model.run(output=True)
+
+sp = openmc.StatePoint('statepoint.100.h5')
+
+fission.load_from_statepoint(sp)
+absorption.load_from_statepoint(sp)
+
+sp.close()
+
+fisxs = fission.get_pandas_dataframe()['mean'].values[0]
+absxs = absorption.get_pandas_dataframe()['mean'].values[0]
+flux = absorption.tallies['flux'].get_pandas_dataframe()['mean'].values[0]
+
+with open('output.txt','w') as file:
+    file.write('fisxs, absxs, flux\n')
+    file.write(f'{fisxs},{absxs},{flux}')
+    file.close()
+
+uni.plot(pixels=400000)
+plt.savefig('geom.png',dpi=600)
+plt.close()
