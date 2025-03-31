ca27ed970af765caa8b4bf5d05e8614f05fdec40
Nathan Glaser <nglaser3@ajnovak2-serv-01.npre.illinois.edu>
2025-03-31 08:57:13 -0500

    depletion reults

--- half-D-model/depletion/half-D.py
+++ half-D-model/depletion/half-D.py
@@ -1,38 +1,26 @@
 import openmc
-import openmc.mgxs
 import openmc.deplete
+
 model = openmc.Model()
-openmc.deplete.pool.NUM_PROCESSES = 1
 materials = openmc.Materials.from_xml('../msbr/msbr-materials.xml')
 
-homogenous = openmc.Material(name = 'homogenous')
-dens = 0.0
-for mat in materials:
-    if mat.name == 'graphite':
-        pass
-    else:
-        for nuc, ao in mat.get_nuclide_atom_densities().items():
-            homogenous.add_nuclide(nuc, ao)
-        dens += mat.density
-        dens_units = mat.density_units
-homogenous.set_density(dens_units, dens/2)
-homogenous.add_s_alpha_beta('c_Graphite',.5)
-homogenous.volume = 3.14*9**2*20
-model.materials = openmc.Materials([homogenous])
+fuel = materials[0]
+fuel.add_nuclide('U233',0.025,'wo')
+model.materials = openmc.Materials([fuel])
 
-bottom, top = [openmc.ZPlane(z0 = v, boundary_type = 'vacuum') for v in [-10,10]] 
-cyl = openmc.ZCylinder(r=9,boundary_type = 'reflective')
-cell = openmc.Cell(name = 'core', fill = homogenous, region= -cyl & +bottom & -top)
+bottom, top = [openmc.ZPlane(z0 = v, boundary_type = 'reflective') for v in [-1000,1000]] 
+cyl = openmc.ZCylinder(r=900,boundary_type = 'reflective')
+cell = openmc.Cell(name = 'core', fill = fuel, region= -cyl & +bottom & -top)
 uni = openmc.Universe(cells = [cell])
 model.geometry.root_universe = uni
 
-model.settings.particles = 1000
+model.settings.particles = 10000
 model.settings.batches = 100
 model.settings.inactive = 50
-model.settings.source = openmc.IndependentSource(space=openmc.stats.Box([-9, -9, -10], 
-                                                                        [9, 9, 10]), constraints = {'fissionable':True})
+model.settings.source = openmc.IndependentSource(space=openmc.stats.Box([-900, -900, -1000], 
+                                                                        [900, 900, 1000]), constraints = {'fissionable':True})
 
 operator = openmc.deplete.CoupledOperator(model, 'chain_endfb71_pwr.xml')
-integrator = openmc.deplete.CECMIntegrator(operator=operator, timesteps=[10*n for n in range(1,37)], power=1e9)
+integrator = openmc.deplete.CECMIntegrator(operator=operator, timesteps=[10 for n in range(1,61)], power=1e9)
 
 integrator.integrate(output=False)
